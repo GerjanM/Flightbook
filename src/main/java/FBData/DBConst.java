7b43a95eb6e7b454109fd54690dc18bfc4822443
DESKTOP-GT6JL2R\gerja <gerjan.maas@gmail.com>
2020-07-29 10:20:16 +0200

    database connection + test + controller class

--- src/main/java/FBData/DBConst.java
+++
@@ -1,8 +0,0 @@
-package FBData;
-
-public class DBConst {
-    public static final String DRIVERNAAM = "com.mysql.cj.jdbc.Driver";
-    public static final String URL = "jdbc:mysql://localhost:3306/flightbook?useSSL=false&serverTimezone=UTC";
-    public static final String GEBRUIKERSNAAM = "root";
-    public static final String WACHTWOORD = "SaamAjnat123";
-}
